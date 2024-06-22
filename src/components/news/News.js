7528d0b6a5faf5ffc448ccec01f3d0b25fa898c5
Purav001 <puravsaluja4@gmail.com>
2024-06-22 17:59:38 +0530

    finaled

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -60,7 +60,7 @@ const News = ({ category, searchTerm }) => {
           {records.map((ele) => (
             <div className="card" key={ele.url}>
               <div className="conatiner">
-                <img src={ele.urlToImage} alt="" />
+                <img src={ele.urlToImage == null ? "https://kubrick.htvapps.com/vidthumb/f6865cb1-d77d-4a31-ba83-d57c4b2324d8/4b9c9d8f-ad14-47ea-bcf4-bf24ee0bb1f3.jpg?crop=0.383xw:0.383xh;0.517xw,0.252xh&resize=1200:*" : ele.urlToImage} alt="" />
                 <div className="overlay"></div>
               </div>
               <div className="card-content overlay">
